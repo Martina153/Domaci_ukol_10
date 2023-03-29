9d9fba8f4687f06f16d2b469847330f567040b92
Martina153 <111387397+Martina153@users.noreply.github.com>
2023-03-29 23:30:08 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,24 +1,39 @@
-const updateCounter = (number) => {
-  const valueElm = document.querySelector('.counter__value');
-  valueElm.textContent= String(number).padStart(2, '0');
-};
+'use strict';
 
-let value = 0;
-const btnUp = document.querySelector('.counter__up');
-const btnDown = document.querySelector('.counter__down');
+console.log('funguju!');
 
-btnUp.addEventListener('click', () => {
-  if (value < 99) {
-    value +=1;
-    updateCounter(value);
-  }
-});
+//Umožněte uživateli zadat počet vteřin skrze připravené textové poličko. 
+//K získání hodnoty z políčka použijte vlastnost value. Nezapomeňte obsah políčka zkonvertovat na číslo. 
 
-btnDown.addEventListener('click', () => {
-  if (value > 0) {
-    value -=1;
-    updateCounter(value);
-  }
-});
+let time;
 
-updateCounter(value);
+    const startTimer = (event) => {
+        //formular sa neodosle
+      event.preventDefault();
+      //zada sa pociatocny input a zmeni na integer
+      const timeInput = document.querySelector(".time-input");
+      time = parseInt(timeInput.value);
+      const interval = setInterval(() => {
+        time--;
+        const minutes = Math.floor(time / 60);
+        const seconds = time % 60;
+        document.querySelector(".alarm__minutes").textContent = minutes;
+        document.querySelector(".alarm__seconds").textContent = seconds < 10 ? `0${seconds}` : seconds;
+        if (time === 0) {
+          clearInterval(interval);
+          const audio = new Audio("assets/alarm.mp3");
+          audio.play();
+        }
+      }, 1000);
+        /*if (time > 0) {
+        time--;
+        document.querySelector(".alarm__seconds").textContent=String(time)
+    }
+        if (time <= 0) {
+            clearInterval(interval);
+          const audio = new Audio("assets/alarm.mp3");
+          audio.play();
+        }
+        }, 1000);*/
+    }
+    document.querySelector(".start").addEventListener("click", startTimer);
